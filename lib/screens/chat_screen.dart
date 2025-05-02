ae9fb31251c92cf86d69c0c6898453190d489bca
Sepehr Tavanaiee <iamsrte.pers@gmail.com>
2025-05-02 14:27:25 +0330

    add chat screen for users to be able to send and receive messages

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app/services/auth_service.dart';
+import 'package:chat_app/services/firestore_service.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/widgets/user_avatar.dart';
+import 'package:chat_app/models/chat_model.dart';
+import 'package:chat_app/models/message_model.dart';
+
+class ChatScreen extends StatefulWidget {
+  final String chatName;
+  final Chat chatItem;
+
+  const ChatScreen({super.key, required this.chatName, required this.chatItem});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  String? currentUserId;
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    currentUserId = AuthService().getCurrentUserId();
+    if (currentUserId == null) {
+      _navigateToLogin();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    widget.chatItem.participants.firstWhere((id) => id != currentUserId);
+    return Scaffold(appBar: _buildAppBar(), body: _buildChatBody());
+  }
+
+  AppBar _buildAppBar() {
+    final chatName = widget.chatName;
+    final avatarUrl = widget.chatItem.imageUrl;
+    final lastSeen = 'last seen recently';
+
+    return AppBar(
+      backgroundColor: const Color(0xffe5edec),
+      title: Row(
+        children: [
+          UserAvatar(name: chatName, avatarUrl: avatarUrl),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(chatName, style: const TextStyle(fontSize: 16)),
+              Text(lastSeen, style: const TextStyle(fontSize: 12)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column _buildChatBody() {
+    return Column(
+      children: [Expanded(child: _buildMessageStream()), _buildInputField()],
+    );
+  }
+
+  Widget _buildMessageStream() {
+    return StreamBuilder<List<ChatMessage>>(
+      stream: FirestoreService().getChatMessagesStream(
+        chatId: widget.chatItem.chatId,
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return _buildErrorState(snapshot.error.toString());
+        }
+
+        if (!snapshot.hasData) {
+          return _buildEmptyState();
+        }
+
+        final messages = snapshot.data!;
+        return _buildMessageList(messages);
+      },
+    );
+  }
+
+  Widget _buildMessageList(List<ChatMessage> messages) {
+    final userId = AuthService().getCurrentUserId();
+    // if (messages.isEmpty) {
+    //   return _buildEmptyChatsState();
+    // }
+
+    return ListView.builder(
+      reverse: true,
+      padding: const EdgeInsets.all(8),
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        final message = messages[index];
+        final isMe = message.senderId == userId!;
+        return Align(
+          alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 4),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              color: isMe ? Colors.blue : Colors.grey[300],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              message.text,
+              style: TextStyle(color: isMe ? Colors.white : Colors.black),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildInputField() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      color: Colors.grey[200],
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: const InputDecoration.collapsed(
+                hintText: 'Write a message...',
+              ),
+            ),
+          ),
+          IconButton(icon: const Icon(Icons.send), onPressed: _sendMessage),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage() {
+    final text = _controller.text.trim();
+    final chatId = widget.chatItem.chatId;
+    final userId = AuthService().getCurrentUserId();
+
+    if (text.isEmpty) return;
+    FirestoreService().sendMessage(
+      chatId: chatId,
+      senderId: userId!,
+      text: text,
+    );
+    _controller.clear();
+  }
+
+  Widget _buildErrorState(String error) {
+    return Center(child: Text('Error: $error'));
+  }
+
+  Widget _buildEmptyState() {
+    return Center(child: Text('No message yet!'));
+  }
+
+  void _navigateToLogin() {
+    if (!mounted) return;
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
+}
--- lib/widgets/chat_item.dart
+++ lib/widgets/chat_item.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:chat_app/models/chat_model.dart';
-import 'package:chat_app/services/firestore_service.dart';
 import 'package:chat_app/widgets/user_avatar.dart';
+import 'package:chat_app/services/firestore_service.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/models/chat_model.dart';
 
-class ChatItem extends StatelessWidget {
+class ChatItem extends StatefulWidget {
   final Chat chatItem;
   final String currentUserId;
 
@@ -14,17 +15,34 @@ class ChatItem extends StatelessWidget {
   });
 
   @override
+  State<ChatItem> createState() => _ChatItem();
+}
+
+class _ChatItem extends State<ChatItem> {
+  Chat? chatItem;
+  String? chatName;
+  String? currentUserId;
+
+  @override
+  void initState() {
+    super.initState();
+    chatItem = widget.chatItem;
+    currentUserId = widget.currentUserId;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<String>(
       future: _getChatName(),
       builder: (context, snapshot) {
         return ListTile(
+          onTap: _navigateToChatScreen,
           leading: UserAvatar(
             name:
-                snapshot.connectionState == ConnectionState.waiting
-                    ? "?"
-                    : snapshot.data!,
-            avatarUrl: chatItem.imageUrl,
+            snapshot.connectionState == ConnectionState.waiting
+                ? "?"
+                : snapshot.data!,
+            avatarUrl: chatItem!.imageUrl,
           ),
           title: Text(
             snapshot.connectionState == ConnectionState.waiting
@@ -52,29 +70,31 @@ class ChatItem extends StatelessWidget {
   }
 
   Future<String> _getChatName() async {
-    if (chatItem.name != null) return chatItem.name!;
+    if (chatItem!.name != null) return chatItem!.name!;
 
     final otherIds =
-        chatItem.participants.where((id) => id != currentUserId).toList();
+    chatItem!.participants.where((id) => id != currentUserId).toList();
 
     if (otherIds.isEmpty) return 'You';
 
     final userFutures =
-        otherIds.map((id) => FirestoreService().getUser(userId: id)).toList();
+    otherIds.map((id) => FirestoreService().getUser(userId: id)).toList();
     final users = await Future.wait(userFutures);
 
     return users.map((user) => '${user.firstName} ${user.lastName}').join(', ');
   }
 
   String _getLastMessagePreview() {
-    final lastMessage = chatItem.lastMessage;
+    final lastMessage = chatItem!.lastMessage;
     if (lastMessage == null) return 'No messages yet';
-    return '${lastMessage['sender_id'] == currentUserId ? 'You: ' : ''} ${lastMessage['text']}';
+    return '${lastMessage['sender_id'] == currentUserId
+        ? 'You: '
+        : ''} ${lastMessage['text']}';
   }
 
   String _getMessageTimestamp() {
     final now = DateTime.now();
-    final time = chatItem.updatedAt;
+    final time = chatItem!.updatedAt;
     final difference = now.difference(time);
 
     if (difference.inDays > 7) {
@@ -83,5 +103,13 @@ class ChatItem extends StatelessWidget {
       return '${difference.inDays}d';
     }
     return '${time.hour}:${time.minute.toString().padLeft(2, '0')}';
+  }
+
+  void _navigateToChatScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => ChatScreen(chatName:'koskhol', chatItem: chatItem!)),
+    );
   }
 }
